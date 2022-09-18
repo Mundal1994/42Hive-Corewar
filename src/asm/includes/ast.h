5567f08db8557fd8ef992ba46f0036fc99d2d353
Caruychen <caruychen@gmail.com>
2022-09-18 11:13:14 +0300

    define preliminary ast

---
+++ src/asm/includes/ast.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ast.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caruychen <cchen@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/18 10:48:21 by caruychen         #+#    #+#             */
+/*   Updated: 2022/09/18 11:12:59 by caruychen        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef AST_H
+# define AST_H
+
+
+typedef enum e_type
+{
+	LABEL,
+	STATEMENT,
+	ARGUMENT
+}		t_type;
+
+typedef struct s_node
+{
+	uint8_t	type;
+	char	*token;
+	t_vec	nodes;
+}		t_node;
+
+/* Abstract Syntax Tree */
+typedef struct s_ast
+{
+	t_vec	nodes;
+}		t_ast;
+
+#endif
