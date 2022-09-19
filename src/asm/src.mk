462f05269ebf8dee874f150e13b7f80146a4ca7a
Caruy Chen <cchen@student.hive.fi>
2022-09-19 11:27:31 +0300

    added c files as variables to makefile

--- src/asm/src.mk
+++ src/asm/src.mk
@@ -6,10 +6,12 @@
 #    By: caruychen <cchen@student.hive.fi>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/09/05 17:20:05 by caruychen         #+#    #+#              #
-#    Updated: 2022/09/18 10:21:33 by caruychen        ###   ########.fr        #
+#    Updated: 2022/09/19 11:22:49 by cchen            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 SRC_DIR := ./src
 SRCS := $(SRC_DIR)/main.c
 SRCS += $(SRC_DIR)/validator.c
+SRCS += $(SRC_DIR)/io_read.c
+SRCS += $(SRC_DIR)/cleanup.c
