4a2ab80b2b17d2ce9ee6417fd818fc98b57c017a
Caruy Chen <cchen@student.hive.fi>
2022-09-20 14:44:33 +0300

    start implementing ast and parser

---
+++ src/asm/src/ast.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ast.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/20 14:33:43 by cchen             #+#    #+#             */
+/*   Updated: 2022/09/20 14:33:45 by cchen            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "asm.h"
+
+void	ast_init(t_ast *ast)
+{
+	t_header	*header;
+
+	header = ast->header;
+	header->magic = COREWAR_EXEC_MAGIC;
+	header->prog_size = 0;
+	ft_bzero(header->prog_name, PROG_NAME_LENGTH + 1);
+	ft_bzero(header->comment, COMMENT_LENGTH + 1);
+	if (vec_new(ast->nodes, 1, sizeof(t_node)) == ERROR)
+		exit_error();
+}
---
+++ src/asm/src/parse.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cchen <cchen@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/20 13:45:41 by cchen             #+#    #+#             */
+/*   Updated: 2022/09/20 13:45:44 by cchen            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "asm.h"
+
+static void	parse_init(t_ast syntax_tree,t_vec *buffer)
+{
+
+}
+
+int	parse(t_ast *syntax_tree, t_vec *buffer)
+{
+	parse_init(syntax_tree, buffer);
+}
