a64ba6f505fefb2af8c02ab6741154150327e7e9
Caruychen <caruychen@gmail.com>
2022-09-23 22:42:48 +0300

    defined source startlien and endline

--- src/asm/src/source_line.c
+++ src/asm/src/source_line.c
@@ -6,7 +6,7 @@
 /*   By: caruychen <marvin@42.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 17:08:03 by caruychen         #+#    #+#             */
-/*   Updated: 2022/09/23 17:38:19 by caruychen        ###   ########.fr       */
+/*   Updated: 2022/09/23 22:41:06 by caruychen        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,10 @@
 
 bool	source_endline(t_source source)
 {
-	
+	return (source.curr == '\n' || source.curr = '\0');
 }
-bool	source_startline(t_source source);
+
+bool	source_startline(t_source source)
+{
+	return (source.pos.c == 1);
+}
