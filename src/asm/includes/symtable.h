cb64806be6c5bb4fc3b459b71581375d54e5caf6
Caruy Chen <cchen@student.hive.fi>
2022-09-26 10:26:39 +0300

    preliminary symtable definition

--- src/asm/includes/symtable.h
+++ src/asm/includes/symtable.h
@@ -13,12 +13,39 @@
 #ifndef SYMTABLE_H
 # define SYMTABLE_H
 
-typedef enum s_st_actions
+# include <stdbool.h>
+
+typedef enum s_actions
 {
 	ST_add,
 	ST_subtract
-}		t_st_actions;
+}		t_actions;
 
-typedef void	(*t_st_patch)(uint32_t mem[], );
+typedef void	(*t_patch)(uint32_t mem[], uint32_t b, uint32_t v, t_actions a);
+
+typedef struct s_forwardrefs
+{
+	uint32_t				byte;
+	t_actions				action;
+	struct s_forwardrefs	*nlink;
+}				t_forwardrefs;
+
+typedef struct s_entry
+{
+	t_alfa				name;
+	uint32_t			value;
+	bool				defined;
+	t_forwardrefs		*flink;
+}				t_entry;
+
+typedef t_vec	t_symtable;
+
+void	_symtable_findentry(t_symtable *symtable, char *name, bool *found);
+void	symtable_print(t_symtable symtable);
+void	symtable_enter(t_symtable *symtable, char *name, uint32_t value);
+void	symtable_valueof(t_symtable *symtable, char *name, uint32_t loc,
+			uint32_t *value, t_actions action, bool *undefined);
+void	symtable_outstandingrefs(t_symtable *symtable,
+			uint32_t *mem, t_patch fix);
 
 #endif
