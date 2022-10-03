d5421db5ca38a28eeb8910e58339fb7b0a4dd847
Caruy Chen <cchen@student.hive.fi>
2022-10-03 11:33:36 +0300

    included asssemble.c into compilation

--- src/asm/src.mk
+++ src/asm/src.mk
@@ -6,7 +6,7 @@
 #    By: caruychen <cchen@student.hive.fi>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/09/05 17:20:05 by caruychen         #+#    #+#              #
-#    Updated: 2022/09/30 18:06:07 by cchen            ###   ########.fr        #
+#    Updated: 2022/10/03 10:13:50 by cchen            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,6 +17,7 @@ SRCS += $(SRC_DIR)/source.c
 SRCS += $(SRC_DIR)/source_line.c
 SRCS += $(SRC_DIR)/source_read.c
 SRCS += $(SRC_DIR)/symbol.c
+SRCS += $(SRC_DIR)/assemble.c
 SRCS += $(SRC_DIR)/lexer.c
 SRCS += $(SRC_DIR)/lexer_bools.c
 SRCS += $(SRC_DIR)/lexer_getchar.c
