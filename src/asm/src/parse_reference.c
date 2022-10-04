0acaf43cf3a803fa6b33abf3868a28d38ede5e6f
Caruychen <caruychen@gmail.com>
2022-10-04 08:41:29 +0300

    parse_reference adds position number into the argument

--- src/asm/src/parse_reference.c
+++ src/asm/src/parse_reference.c
@@ -6,7 +6,7 @@
 /*   By: caruychen <marvin@42.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 21:58:37 by caruychen         #+#    #+#             */
-/*   Updated: 2022/10/03 21:59:37 by caruychen        ###   ########.fr       */
+/*   Updated: 2022/10/04 08:27:40 by caruychen        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,10 @@
 int	parse_reference(t_statement *statement, t_lexer *lexer, t_symbols *sym,
 		uint8_t index)
 {
+	if (sym->argtype & T_DIR)
+		statement->arguments[index].dir = statement->pos;
+	if (sym->argtype & T_IND)
+		statement->arguments[index].ind = (uint16_t) statement->pos;
 	if (lexer_next(lexer, sym) == ERROR)
 		return (ERROR);
 	return (parse_arg_end(lexer, sym, statement, index));
