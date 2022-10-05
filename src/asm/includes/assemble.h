7f6d6806ad199c6d27e795bf4a41777fec13a62a
Caruy Chen <caruychen@gmail.com>
2022-10-05 17:05:08 +0300

    preliminary assemble buffer

--- src/asm/includes/assemble.h
+++ src/asm/includes/assemble.h
@@ -21,8 +21,10 @@ typedef struct	s_assembler
 	t_string	buffer;
 }				t_assembler;
 
-void	assemble_free(t_assembler *assembler, t_parser *parser);
-void	assemble(const char *arg);
-void	assemble_buffer(t_assembler *assembler, t_parser *parser);
+void		assemble_free(t_assembler *assembler, t_parser *parser);
+void		assemble(const char *arg);
+void		assemble_buffer(t_assembler *assembler, t_parser *parser);
+t_string	*assemble_push_int(t_string *buffer, uint32_t num);
+t_string	*assemble_push_num(t_string *buffer, t_arg arg, uint8_t size);
 
 #endif
--- src/asm/src/assemble_buffer.c
+++ src/asm/src/assemble_buffer.c
@@ -12,38 +12,6 @@
 
 #include "asm.h"
 
-static t_string	*push_null(t_string *buffer)
-{
-	if (!buffer || !buffer->capacity)
-		return (NULL);
-	if (buffer->length >= buffer->capacity - 1)
-		if (!string_resize(buffer, buffer->capacity * STR_RESIZE_FACTOR))
-			return (NULL);
-	++buffer->length;
-	return (buffer);
-}
-
-t_string	*assemble_push_int(t_string *buffer, uint32_t num)
-{
-	size_t	size;
-	char	byte;
-
-	size = 4;
-	while (size > 0)
-	{
-		byte = num >> (BITS * --size);
-		if (byte == 0)
-		{
-			if (!push_null(buffer))
-				return (NULL);
-			continue ;
-		}
-		if (!string_push(buffer, byte))
-			return (NULL);
-	}
-	return (buffer);
-}
-
 static void buff_header(t_assembler *assembler, t_parser *parser)
 {
 	t_string	*buffer;
@@ -60,8 +28,69 @@ static void buff_header(t_assembler *assembler, t_parser *parser)
 	exit_error();
 }
 
+uint8_t	acb_flag(uint8_t acb, uint8_t index)
+{
+	uint8_t	flag;
+	uint8_t	shifts;
+
+	shifts = 2 * (3 - index);
+	flag = 3 << shifts;
+	acb &= flag;
+	return (acb >> shifts);
+}
+
+static int	buff_statement(t_string *buffer, t_statement *statement)
+{
+	t_op	op;
+	uint8_t	index;
+	uint8_t	acb;
+	uint8_t	size;
+
+	op = statement->op;
+	acb = statement->acb;
+	if (!string_push(buffer, op.opcode))
+		return (ERROR);
+	if (op.acb && !string_push(buffer, acb))
+		return (ERROR);
+	index = 0;
+	while (index < op.argc)
+	{
+		if (acb_flag(acb, index) == DIR_CODE)
+			size = 2 + 2 * !op.label;
+		if (acb_flag(acb, index) == IND_CODE)
+			size = 2;
+		if (acb_flag(acb, index) == REG_CODE)
+			size = 1;
+		if (!assemble_push_num(buffer, statement->arguments[index++], size));
+			return (ERROR);
+	}
+	return (OK);
+}
+
+static void	buff_body(t_assembler *assembler, t_parser *parser)
+{
+	t_string	*buffer;
+	t_vec		*code;
+	t_statement	*statement;
+	size_t		index;
+
+	buffer = &assembler->buffer;
+	code = &parser->body;
+	index = 0;
+	while (index < code->len)
+	{
+		statement = (t_statement *) vec_get(&parser->body, index);
+		if (buff_statement(buffer, statement) == ERROR);
+		{
+			assemble_free(assembler, parser);
+			exit_error();
+		}
+		++index;
+	}
+}
+
 void	assemble_buffer(t_assembler *assembler, t_parser *parser)
 {
 	buff_header(assembler, parser);
-	buff_body(t_assembler *assembler, t_parser *parser);
+	buff_body(assembler, parser);
 }
