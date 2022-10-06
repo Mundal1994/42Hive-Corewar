0ddc61605c990d0817cf0342047b5a7b362ab24d
Caruy Chen <caruychen@gmail.com>
2022-10-06 15:34:26 +0300

    fix bug with math operators and numbers

--- src/asm/src/lexer_getchar.c
+++ src/asm/src/lexer_getchar.c
@@ -17,8 +17,14 @@ int	lexer_getchar(t_source *source, t_symbols *sym)
 	char	*curr;
 
 	curr = source->curr;
-	if (*curr >= '+' && *curr <= '-')
-		sym->type = LA_plus + (*curr - '+');
+	if (is_operator(*curr) && ft_isdigit(*source_peek(source)))
+		return (lexer_getnumber(source, sym));
+	if (*curr == '+')
+		sym->type = LA_plus;
+	if (*curr == ',')
+		sym->type = LA_comma;
+	if (*curr == '-')
+		sym->type = LA_minus;
 	source_next(source);
 	if (!string_replace_n(&sym->str, curr, 1))
 		return (ERROR);
