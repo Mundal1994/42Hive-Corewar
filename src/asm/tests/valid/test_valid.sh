acba25b25e47660e2d5c000d8b75d7c393133267
Caruy Chen <caruychen@gmail.com>
2022-10-06 11:33:04 +0300

    prelim test script

--- src/asm/tests/valid/test_valid.sh
+++ src/asm/tests/valid/test_valid.sh
@@ -3,27 +3,38 @@
 RED='\033[0;31m'
 NC='\033[0m'
 GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
 DIR=$(dirname `which $0`)
 cd $DIR
 
-LEAKS () {
+RUN_ASM () {
+	echo "${YELLOW}Running our asm, & checking leaks${NC}"
+	COR=$(echo $1 | sed 's/\.s/\.cor/g')
+	leaks ../../asm $1
 	leaks --atExit -- ../../asm $1 &> res
 	if tail -n2 res | head -n1 | grep -q '0 leaks for 0 total leaked bytes.'; then
 		echo "${GREEN}No leaks.${NC}"
 	else
 		echo "${RED}LEAKS!!!${NC}"
 	fi
-	/bin/rm res
+#	/bin/rm res
+#	/bin/rm $COR
 }
 
 RUN_HIVE_ASM () {
-	echo $1
+	echo "${YELLOW}Running school asm${NC}"
+	COR=$(echo $1 | sed 's/\.s/\.cor/g')
+	HIVE=$(echo $1 | sed 's/\.s/\.hive/g')
 	./hive_asm $1
-	rm $1
+	echo "Renaming: $COR -> $HIVE\n"
+	/bin/mv $COR $HIVE
+	/bin/rm $HIVE
 }
 
 echo "Testing valid maps..."
 for f in $(find . -type f -name "*.s"); do
+	echo $f
 	RUN_HIVE_ASM $f
+	RUN_ASM $f
 	echo ""
 done
