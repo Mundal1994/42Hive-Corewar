ace0e9fb689a52af70f6610c1a39020cd6858222
Caruy Chen <caruychen@gmail.com>
2022-10-06 13:52:28 +0300

    basic test shell script for valid files

--- src/asm/tests/valid/test_valid.sh
+++ src/asm/tests/valid/test_valid.sh
@@ -7,34 +7,58 @@ YELLOW='\033[1;33m'
 DIR=$(dirname `which $0`)
 cd $DIR
 
-RUN_ASM () {
-	echo "${YELLOW}Running our asm, & checking leaks${NC}"
-	COR=$(echo $1 | sed 's/\.s/\.cor/g')
-	leaks ../../asm $1
+CHECK_LEAKS () {
+	echo -n "Leaks: "
 	leaks --atExit -- ../../asm $1 &> res
 	if tail -n2 res | head -n1 | grep -q '0 leaks for 0 total leaked bytes.'; then
 		echo "${GREEN}No leaks.${NC}"
 	else
 		echo "${RED}LEAKS!!!${NC}"
 	fi
-#	/bin/rm res
-#	/bin/rm $COR
+	/bin/rm res
+	/bin/rm $2
+}
+
+RUN_ASM () {
+	echo "Running our asm:"
+	CHECK_LEAKS $1 $2
+	../../asm $1
+	echo ""
 }
 
 RUN_HIVE_ASM () {
-	echo "${YELLOW}Running school asm${NC}"
-	COR=$(echo $1 | sed 's/\.s/\.cor/g')
-	HIVE=$(echo $1 | sed 's/\.s/\.hive/g')
+	echo "Running school asm:"
 	./hive_asm $1
-	echo "Renaming: $COR -> $HIVE\n"
-	/bin/mv $COR $HIVE
-	/bin/rm $HIVE
+	echo "Renaming: $2 -> $3\n"
+	/bin/mv $2 $3
+}
+
+COMPARE () {
+	COR=$1
+	HIVE=$2
+	DIFF_FILE=$(echo $1 | sed 's/\.cor/\.diff/g')
+	echo "Running comparison:"
+	hexdump -vC $COR > dumpcor
+	hexdump -vC $HIVE > dumphive
+	DIFF=$(diff dumpcor dumphive)
+	if [ "$DIFF" != "" ]
+	then
+		echo "${RED}Found a difference${NC}"
+		echo $DIFF > $DIFF_FILE
+	else
+		echo "${GREEN}Outputs identical${NC}"
+		/bin/rm $COR $HIVE
+	fi
+	/bin/rm dumpcor dumphive
 }
 
 echo "Testing valid maps..."
 for f in $(find . -type f -name "*.s"); do
-	echo $f
-	RUN_HIVE_ASM $f
-	RUN_ASM $f
+	echo "${YELLOW}$f${NC}"
+	COR=$(echo $f | sed 's/\.s/\.cor/g')
+	HIVE=$(echo $f | sed 's/\.s/\.hive/g')
+	RUN_HIVE_ASM $f $COR $HIVE
+	RUN_ASM $f $COR
+	COMPARE $COR $HIVE
 	echo ""
 done
