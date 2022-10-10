d9d13d84d9b6576d22bec8012940da38d4f628a0
Caruy Chen <caruychen@gmail.com>
2022-10-10 11:31:23 +0300

    warn on empty code

--- src/asm/src/assemble.c
+++ src/asm/src/assemble.c
@@ -18,6 +18,8 @@ static void	assemble_init(t_assembler *assembler, t_parser *parser,
 	size_t	len;
 	size_t	cor_size;
 
+	if (parser->size == 0)
+		warning_ret(ASSEMBLE_EMPTY_CODE);
 	assembler->parser = parser;
 	len = ft_strrchr(arg, '.') - arg + 1;
 	cor_size = 8 + PROG_NAME_LENGTH + COMMENT_LENGTH + parser->size;
