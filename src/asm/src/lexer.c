3c68dbabcb7ead85f5c8860ee4e366ca2f292d85
Caruy Chen <caruychen@gmail.com>
2022-10-17 14:53:39 +0300

    document lexer.c

--- src/asm/src/lexer.c
+++ src/asm/src/lexer.c
@@ -10,9 +10,28 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/* ************************************************************************** *
+ * Lexical analysis
+ *   - Deals with  the "words"
+ *   - Converts the sequence of characters into a sequence symbols (tokens).
+ *   - The source characters can be assembled into one of 14 different types
+ *     of symbols, such as a label, comma, operation, argument etc. The full
+ *     list of symbol types can be found in symbol.h
+ *   - The lexer does not understand any grammatical context, it just tells you
+ *     what the next word is.
+ *   - Reports an error if it finds any unrecognised symbols. So the client
+ *     won't have to worry about "word correctness"
+ *
+ * The main interface to the lexer is the "lexer_next" function, found in
+ * lexer_next.c
+ *
+ * Function found in this file serve mainly as utility functions.
+ * ************************************************************************* */
+
 #include "definitions.h"
 #include "lexer.h"
 
+/* Initialises source data, and reads text into the buffer */
 void	lexer_init(t_lexer *lexer, const char *filename)
 {
 	source_init(&lexer->source);
@@ -20,11 +39,14 @@ void	lexer_init(t_lexer *lexer, const char *filename)
 	source_next(&lexer->source);
 }
 
+/* Frees memory from source */
 void	lexer_free(t_lexer *lexer)
 {
 	source_free(&lexer->source);
 }
 
+/* If the next symbol is end of the line, searches for the start of the
+ * next line. Otherwise, just returns the next symbol */
 int	lexer_nextline(t_lexer *lexer, t_symbols *sym)
 {
 	int	res;
