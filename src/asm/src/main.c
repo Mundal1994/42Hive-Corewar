959f54e1c2086dbec32e93df72d01849790c2b9e
Caruychen <caruychen@gmail.com>
2022-10-17 10:16:29 +0300

    more comments in main.c

--- src/asm/src/main.c
+++ src/asm/src/main.c
@@ -6,7 +6,7 @@
 /*   By: caruychen <cchen@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 17:16:33 by caruychen         #+#    #+#             */
-/*   Updated: 2022/10/17 10:10:27 by caruychen        ###   ########.fr       */
+/*   Updated: 2022/10/17 10:16:12 by caruychen        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,13 @@
  *   - Statement vector is unpacked into bytecode, and loaded into a buffer.
  *   - Once all statements are unpacked, the buffer is finally written to
  *     the resulting binary file.
+ *
+ * The Parse stage involves multiple phases of input processing:
+ * 1. Source handling
+ * 2. Lexical analysis
+ * 3. Syntax analysis
+ *   - Symbol table handling
+ * More details for the parsing staged outlined in parse.c
 */
 int	main(int argc, char **argv)
 {
