f7ceef4129212d6d55f9556f6148cfe78123ee92
Caruy Chen <caruychen@gmail.com>
2022-10-20 13:56:01 +0300

    prelim test file

--- tests/master.sh
+++ tests/master.sh
@@ -21,22 +21,84 @@ RUN_CMD () {
 	READ_YN
 }
 
+RET_YN () {
+	while true
+	do
+		read -s -k "REPLY"
+		if [ ${REPLY} = "y" ]
+		then
+			return true
+		elif [ ${REPLY} = "n" ]
+		then
+			return false
+		fi
+	done
+}
+
 READ_YN () {
-	read -s -k "REPLY"
-	if [ ${REPLY} = "y" ]
+	if RET_YN false
 	then
-		tput rc
-		tput el
+##		tput rc && tput cd
+		echo "${GREEN}YES${NC}"
 	else
-		echo ${REPLY}
+#		tput rc && tput cd
+		echo "${RED}NO${NC}"
 	fi
 }
 
+NORM_MANUAL () {
+	tput sc
+	echo "${YELLOW}Would you like to see the norminette for yourself? [y/n]${NC}"
+	while true
+	do
+		read -s -k "REPLY"
+		if [ ${REPLY} = "y" ]
+		then
+			norminette ../
+			echo "${YELLOW}Are you satisfied? [y/n]${NC}"
+			if RET_YN
+			then
+				tput clear
+				echo "- Evaluator is satisfied with the norme: ${GREEN}YES${NC}"
+			else
+				tput clear
+				echo "- Evaluator is satisfied with the norme: ${RED}NO, BOOHOO${NC}"
+			fi
+			break
+		elif [ ${REPLY} = "n" ]
+		then
+			tput rc && tput cd
+			break
+		fi
+	done
+
+}
+
+NORMINETTE () {
+	tput sc
+	echo "${YELLOW}Auto-testing 'norminette ../'${NC}"
+	res=$(norminette ../)
+	tput rc && tput cd
+	if echo ${res} | grep -q 'Error'
+	then
+		echo "${RED}NO${NC}"
+	else
+		echo "${GREEN}OK${NC}"
+	fi
+	NORM_MANUAL
+}
+
+tput clear
 echo "Let's stop these mother@#$%in snakes on this mother@#$%in helltrain!\n"
 
 echo "Welcome to the Corewar test, let's begin with the basics:"
 READ_CONT
 
-echo "- Something was submitted: ${GREEN}YES${NC}"
+echo -n "- Something was submitted: "
+RUN_CMD "\n${YELLOW}Running 'ls' from root, can you see stuff? [y/n]${NC}" "ls -al ../"
 echo -n "- Author file repository: "
 RUN_CMD "\n${YELLOW}Running 'ls' from root, can you see the author file? [y/n]${NC}" "ls -al ../"
+echo -n "- Correct author file contents: "
+RUN_CMD "\n${YELLOW}Printing author file with 'car -e', is if formatted correctly? [y/n]${NC}" "cat -e ../author"
+echo -n "- Norm is OK: "
+NORMINETTE
